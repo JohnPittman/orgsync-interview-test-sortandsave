74509a4c4a00aba7cc1566a2cba7b87ef26c71eb
John Pittman <johnrichadrpittman@gmail.com>
2015-08-05 20:49:17 -0400

    Complete. Basic UI.

---
+++ README.md
@@ -0,0 +1,27 @@
+Given the following HTML provide a JavaScript function that takes first name, last name
+and status as arguments, inserts a new table row into the markup below sorted by name
+and saves the information to the database. You can modify the HTML as needed. You can
+assume that sending data to the URL, “/member/save”, will save the name and status.
+Please list any open source JavaScript libraries used in this solution.
+
+<table>
+<thead>
+<tr>
+<th>Name</th>
+<th>Status</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Doe, John</td>
+<td>Approved</td>
+</tr>
+</tbody>
+</table>
+
+#Assumptions  
+
+ - Raw JavaScript.
+ - Just the basics.
+ - Sending JSON, receiving text or JSON response.
+ - The save part of this test is just pseudo and I don't need to open up a connection to a server to test the route.
---
+++ src/app.js
@@ -0,0 +1,181 @@
+'use strict';
+
+/*
+Mock Data
+ */
+
+var peopleData = [{
+    firstName: 'John',
+    lastName: 'Doe',
+    status: 'Denied'
+}, {
+    firstName: 'John',
+    lastName: 'Doe',
+    status: 'Approved'
+}];
+
+/*
+View Components
+ */
+
+/**
+ * View component to display a person.
+ * 
+ * @param {string} firstName
+ * @param {string} lastName
+ * @param {string} status
+ */
+function PersonComponent(firstName, lastName, status) {
+    this.firstName = formatProperNoun(firstName);
+    this.lastName = formatProperNoun(lastName);
+    this.status = formatProperNoun(status);
+}
+
+PersonComponent.prototype.render = function() {
+    var el = document.createElement('tr');
+    el.innerHTML =
+        '<td>' + this.lastName + ', ' + this.firstName + '</td>' +
+        '<td>' + this.status + '</td>';
+    return el;
+};
+
+PersonComponent.prototype.getFullName = function() {
+    return this.lastName + ', ' + this.firstName;
+};
+
+/*
+App Load
+ */
+
+var personComponents = [];
+
+document.addEventListener('DOMContentLoaded', init());
+
+function init() {
+    // populate component list from existing data.
+    for (var i = 0, n = peopleData.length; i < n; ++i) {
+        var personData = peopleData[i];
+        personComponents.push(new PersonComponent(personData.firstName, personData.lastName, personData.status));
+    }
+
+    // sort
+    sortPersonComponents();
+
+    // render initial table
+    renderTable();
+
+    // hook up events
+    var form=document.forms[0];
+
+    form.addEventListener('input',function(e){
+    	this.firstName.value = formatProperNoun(this.firstName.value);
+    	this.lastName.value = formatProperNoun(this.lastName.value);
+    	this.status.value = formatProperNoun(this.status.value);
+    });
+
+    form.addEventListener('submit', function(e) {
+    	e.preventDefault();
+    	e.stopPropagation();
+
+        addPerson(this.firstName.value, this.lastName.value, this.status.value);
+
+        this.reset();
+    });
+}
+
+/*
+Functionality Methods
+ */
+
+function formatProperNoun(str) {
+    str = str || '';
+
+    var properNoun = str.charAt(0).toUpperCase();
+    if (str.length > 1) {
+        properNoun += str.substring(1).toLowerCase();
+    }
+
+    return properNoun;
+}
+
+function clearTable() {
+    var peopleContainer = document.getElementsByTagName('tbody')[0];
+
+    while (peopleContainer.firstChild !== null) {
+        peopleContainer.removeChild(peopleContainer.firstChild);
+    }
+}
+
+function renderTable() {
+    var peopleContainer = document.getElementsByTagName('tbody')[0];
+    for (var i = 0, n = personComponents.length; i < n; ++i) {
+        var personComponent = personComponents[i];
+        peopleContainer.appendChild(personComponent.render());
+    }
+}
+
+function sortPersonComponents() {
+    // sort
+    personComponents.sort(function(a, b) {
+        var fullNameA = a.getFullName();
+        var fullNameB = b.getFullName();
+
+        if (fullNameA < fullNameB)
+            return -1;
+        else if (fullNameA === fullNameB) {
+            if (a.status < b.status)
+                return -1;
+            return 1;
+        } else
+            return 1;
+    });
+}
+
+function addPerson(firstName, lastName, status) {
+    // Doing this since it's a tiny app otherwise I would utilize DOM references and insert into the table vs re-rendering all.
+    // Programming in a React, component, or game design fashion is much cleaner and fun.
+    clearTable();
+
+    // this would normally be done as part of a table component.
+    // for the example only.
+    var person = new PersonComponent(firstName, lastName, status);
+    personComponents.push(person);
+    sortPersonComponents();
+
+    // re-render table
+    renderTable();
+
+    // save person to server
+    savePerson(person).then(function(responseText) {
+        console.log('save success: ' + responseText);
+    }).catch(function(statusText) {
+        console.log('save error status: ' + statusText);
+    });
+}
+
+/*
+AJAX
+ */
+
+// simple form of javascript ajax without using third-party module.
+// I built a simple http module a while back like so many others to not be buried in jquery
+// but after gaining massive amounts of knowledge I will be making a new lightwieght PreloadJS style one when I get a few days
+// for getting into the future of WebGL assets and custom size loading.
+function savePerson(personData) {
+    var promise = new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+
+        xhr.onload = function() {
+            if (this.status === 200 ||
+                this.status === 4) {
+                resolve(JSON.parse(this.responseText));
+            } else {
+                reject(this.statusText);
+            }
+        };
+
+        xhr.open('post', '/member/save”', true);
+        xhr.send(JSON.stringify(personData));
+    });
+    return promise;
+}
